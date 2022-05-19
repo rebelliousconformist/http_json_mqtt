9ca3cc558362dda1e67d96810d1016e77ebeea5a
Nachiket Bapat <nacbapat@gmail.com>
2022-05-19 17:48:41 +0200

    add https support

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ int main(int, char**) {
     try
     {
         std::cout << "Program start \n";
-        queryUrl call("http://192.168.10.163");    
-        call.queryBaseAndPrint();
+        queryUrl call("api.openweathermap.org");
+        const std::string endpointotcall("/data/2.5/weather?lat=48.366512&lon=10.894446&appid=b05b2fd9019f11c4bf46a71177ec2d15");
+        call.callAndSaveResponse();
 
     }
     catch(const std::exception& e)
--- query.h
+++ query.h
@@ -2,31 +2,46 @@
 #include "json.hpp"
 
 using json = nlohmann::json ;
-
 class queryUrl
 {
 private:
     httplib::Client *cli;       //this resource is being init in the constructor, hence rule of 3 needs to be implemented
-    std::string urlToCall;
-    std::string responseBodyData;
+    httplib::SSLClient *cli_https;
+    std::string mUrlToCall;     //base Url
+    std::string mEndpointToCall;//end Point
+    std::string mResponseData;  //responsedata
 public:
+    queryUrl() = delete;        //disable default constructor, it has to be init with url or copied
     queryUrl(std::string url);
     queryUrl(const queryUrl& other);        //copy constructor
     queryUrl& operator=(const queryUrl& other);
-    void queryBaseAndPrint();
+    void setEndpointToCall(const std::string &);
+    void callAndSaveResponse();
     void queryConvertToJson();
     ~queryUrl();
 };
 
-queryUrl::queryUrl(std::string url):urlToCall(url)
+queryUrl::queryUrl(std::string url):mUrlToCall(url)
 {
-    this->cli = new httplib::Client(this->urlToCall);
+    //this->cli = new httplib::Client(this->mUrlToCall);
+
+    #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+        cli_https = new httplib::SSLClient(this->mUrlToCall);
+        cli_https->enable_server_certificate_verification(true);
+    #else
+        cli = new httplib::Client(this->mUrlToCall);
+    #endif
 }
 
 //copy constructor to avoid shallow copy
-queryUrl::queryUrl(const queryUrl& other):urlToCall(other.urlToCall){
+queryUrl::queryUrl(const queryUrl& other):mUrlToCall(other.mUrlToCall){
 
-    this->cli = new httplib::Client(this->urlToCall);
+    #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+        cli_https = new httplib::SSLClient(this->mUrlToCall);
+        cli_https->enable_server_certificate_verification(true);
+    #else
+        cli = new httplib::Client(this->mUrlToCall);
+    #endif
 }
 
 queryUrl& queryUrl::operator=(const queryUrl& other){
@@ -35,18 +50,36 @@ queryUrl& queryUrl::operator=(const queryUrl& other){
     }
     else
     {
-    this->urlToCall = other.urlToCall;
-    this->cli = new httplib::Client(this->urlToCall);
+    this->mUrlToCall = other.mUrlToCall;
+
+    #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+        cli_https = new httplib::SSLClient(this->mUrlToCall);
+        cli_https->enable_server_certificate_verification(true);
+    #else
+        cli = new httplib::Client(this->mUrlToCall);
+    #endif
+
     return *this;
     }
 }
 
+void queryUrl::setEndpointToCall(const std::string &endpoint){
+
+    this->mEndpointToCall = endpoint;
+    
+}
 
 
-void queryUrl::queryBaseAndPrint(){
-    if(auto getResponse = this->cli->Get("/iolinkmaster/port[1]/iolinkdevice/pdin/getdata")){
+void queryUrl::callAndSaveResponse(){
+
+    #ifdef CPPHTTPLIB_OPENSSL_SUPPORT
+        if(auto getResponse = this->cli_https->Get(mEndpointToCall.c_str()))
+    #else
+        if(auto getResponse = this->cli->Get(mEndpointToCall.c_str()))
+    #endif
+    {
     std::cout << "string response is : " << getResponse->body << std::endl;
-    responseBodyData = getResponse->body;
+    mResponseData = getResponse->body;
     }
     else
     {
@@ -55,13 +88,12 @@ void queryUrl::queryBaseAndPrint(){
 }
 
 void queryUrl::queryConvertToJson(){
-    json responseJson = json::parse(responseBodyData);
+    json responseJson = json::parse(mResponseData);
     std::cout << "size" << responseJson.size() <<  "\n";
     for (size_t i = 0; i < responseJson.size(); i++)
     {
         std::cout << "count is" <<  responseJson.at(i).value("age",-1) << "\n";
     }
-  
 }
 
 queryUrl::~queryUrl()
@@ -71,5 +103,4 @@ queryUrl::~queryUrl()
         delete cli;
         cli = nullptr;
     }
-
 }
